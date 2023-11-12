33e5c8f14aa81f89de5f598ec6bb2ba15e73a6ea
bharadwajsirigadi <bharadwajsirigadi@gmail.com>
2023-11-11 19:12:54 -0500

    clean up

--- kitti_odometry_bag_generator/kitti.py
+++ kitti_odometry_bag_generator/kitti.py
@@ -15,10 +15,11 @@ from kitti_odometry_bag_generator.utils.kitti_utils import KITTIOdometryDataset
 from kitti_odometry_bag_generator.utils.quaternion import Quaternion
 # from pathlib import Path
 from nav_msgs.msg import Odometry
-import tf2_ros
+from tf2_ros import TransformListener, Buffer
 
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
+from tf2_ros.static_transform_broadcaster import StaticTransformBroadcaster
 
 DATASET_DIR = "/media/psf/SSD/DRONES_LAB/kitti_dataset/dataset"
 ODOM_DIR = '/media/psf/SSD/DRONES_LAB/kitti_dataset/dataset_2'
@@ -28,24 +29,23 @@ class Kitti_Odom(Node):
     def __init__(self, data_dir, odom_dir, sequence: int):
         super().__init__("kitti_odom")
         self.kitti_dataset = KITTIOdometryDataset(data_dir, odom_dir, sequence)
-        # self.timer = self.create_timer(0.5, self.send_velocity_command)
+        self.bridge = CvBridge()
+
+        self.counter = 0
+        self.counter_limit = 4540
+        
         self.left_imgs = self.kitti_dataset.left_images()
         self.right_imgs = self.kitti_dataset.right_images()
         self.times_file = self.kitti_dataset.times_file()
         self.ground_truth = self.kitti_dataset.odom_pose()
-        self.counter = 0
-        self.counter_limit = 4540
 
-        self.bridge = CvBridge()
-
+        self.timer = self.create_timer(0.05, self.publish_callback)
         self.left_img_publisher_ = self.create_publisher(Image, '/camera1/left/image_raw', 10)
         self.right_img_publisher_ = self.create_publisher(Image, '/camera2/right/image_raw', 10)
         self.left_camera_info_publisher = self.create_publisher(CameraInfo, '/camera1/left/camera_info', 10)
         self.right_camera_info_publisher = self.create_publisher(CameraInfo, '/camera2/right/camera_info', 10)
         self.odom_publisher = self.create_publisher(Odometry,'/car_1/base/odom', 10)
         self.path_publisher = self.create_publisher(Path, '/car_1/path', 10)
-        self.p_msg = Path()
-        self.timer = self.create_timer(0.05, self.publish_callback)
 
     def publish_callback(self):
         # retrieving images and creating msg
@@ -61,35 +61,17 @@ class Kitti_Odom(Node):
         odom_msg.header.stamp.nanosec = int((clock_time - int(clock_time)) * 1e9)
 
         # retrieving and creating ground truth msg
-        # print(self.ground_truth[self.counter])
         quaternion = Quaternion(self.ground_truth[self.counter])
         x, y, z, w = quaternion.transformation_to_quaternion()
-
         translation = self.ground_truth[self.counter][:3,3]
-        odom_msg.header.frame_id = 'map'
-        odom_msg.child_frame_id = 'base_link'
 
-        odom_msg.pose.pose.position.x = translation[0]
+        odom_msg.pose.pose.position.z = translation[0]
         odom_msg.pose.pose.position.y = translation[1] 
-        odom_msg.pose.pose.position.z = translation[2] 
+        odom_msg.pose.pose.position.x = translation[2] 
         odom_msg.pose.pose.orientation.x = x
-        odom_msg.pose.pose.orientation.y = z
-        odom_msg.pose.pose.orientation.z = y
+        odom_msg.pose.pose.orientation.y = y
+        odom_msg.pose.pose.orientation.z = z
         odom_msg.pose.pose.orientation.w = w
-
-        pose = PoseStamped()
-        pose.pose.position.x = translation[0]
-        pose.pose.position.z = translation[1]
-        pose.pose.position.y = translation[2]
-        pose.pose.orientation.z = x
-        pose.pose.orientation.y = y
-        pose.pose.orientation.x = z
-        pose.pose.orientation.w = w
-        # pose.header.frame_id = "map"
-        
-        self.p_msg.poses.append(pose)
-        self.p_msg.header.frame_id = "map"
-        self.path_publisher.publish(self.p_msg)
 
         self.get_logger().info(f'{self.counter}-Images Processed')
 
@@ -103,7 +85,6 @@ class Kitti_Odom(Node):
 
         self.counter += 1
         return
-
 
 def main(args=None):
     rclpy.init(args=args)
