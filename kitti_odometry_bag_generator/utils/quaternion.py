5a77583d4662c6043f7a743f994b9a9206b063ff
bharadwajsirigadi <bharadwajsirigadi@gmail.com>
2023-11-23 11:05:20 -0500

    quaternion.py clean up

--- kitti_odometry_bag_generator/utils/quaternion.py
+++ kitti_odometry_bag_generator/utils/quaternion.py
@@ -10,64 +10,112 @@ class Quaternion():
         pass
 
     def transformation_to_quaternion(self):
-        r11 = self.rotation_matrix[0,0]
-        r12 = self.rotation_matrix[0,1]
-        r13 = self.rotation_matrix[0,2]
-        r21 = self.rotation_matrix[1,0]
-        r22 = self.rotation_matrix[1,1]
-        r23 = self.rotation_matrix[1,2]
-        r31 = self.rotation_matrix[2,0]
-        r32 = self.rotation_matrix[2,1]
-        r33 = self.rotation_matrix[2,2]
+        # r11 = self.rotation_matrix[0,0]
+        # r12 = self.rotation_matrix[0,1]
+        # r13 = self.rotation_matrix[0,2]
+        # r21 = self.rotation_matrix[1,0]
+        # r22 = self.rotation_matrix[1,1]
+        # r23 = self.rotation_matrix[1,2]
+        # r31 = self.rotation_matrix[2,0]
+        # r32 = self.rotation_matrix[2,1]
+        # r33 = self.rotation_matrix[2,2]
 
-        q_0n = ((1+r11+r22+r33)/4)
-        q_1n = ((1+r11-r22-r33)/4)
-        q_2n = ((1-r11+r22-r33)/4)
-        q_3n = ((1-r11-r22+r33)/4)
+        # q_0n = ((1+r11+r22+r33)/4)
+        # q_1n = ((1+r11-r22-r33)/4)
+        # q_2n = ((1-r11+r22-r33)/4)
+        # q_3n = ((1-r11-r22+r33)/4)
 
-        if q_0n > 0:
-            q_0 = abs(math.sqrt((1+r11+r22+r33)/4))
-        else:
-            q_0 = abs(math.sqrt(-(1+r11+r22+r33)/4))
+        # if q_0n > 0:
+        #     q_0 = abs(math.sqrt((1+r11+r22+r33)/4))
+        #     q_0k = math.sqrt((1+r11+r22+r33)/4)
+        # else:
+        #     q_0 = abs(math.sqrt(-(1+r11+r22+r33)/4))
+        #     q_0k = -(math.sqrt(-(1+r11+r22+r33)/4))
         
-        if q_1n > 0:
-            q_1 = abs(math.sqrt((1+r11-r22-r33)/4))
-        else:
-            q_1 = abs(math.sqrt(-(1+r11-r22-r33)/4))
+        # if q_1n > 0:
+        #     q_1 = abs(math.sqrt((1+r11-r22-r33)/4))
+        #     q_1k = q_1
+        # else:
+        #     q_1 = abs(math.sqrt(-(1+r11-r22-r33)/4))
+        #     q_1k = -(math.sqrt(-(1+r11-r22-r33)/4))
         
-        if q_2n > 0:
-            q_2 = abs(math.sqrt((1-r11+r22-r33)/4))
-        else:
-            q_2 = abs(math.sqrt(-(1-r11+r22-r33)/4))
+        # if q_2n > 0:
+        #     q_2 = abs(math.sqrt((1-r11+r22-r33)/4))
+        #     q_2k = q_2
+        # else:
+        #     q_2 = abs(math.sqrt(-(1-r11+r22-r33)/4))
+        #     q_2k = -(math.sqrt(-(1-r11+r22-r33)/4))
         
-        if q_3n > 0:
-            q_3 = abs(math.sqrt((1-r11-r22+r33)/4))
-        else:
-            q_3 = abs(math.sqrt(-(1-r11-r22+r33)/4))
+        # if q_3n > 0:
+        #     q_3 = abs(math.sqrt((1-r11-r22+r33)/4))
+        #     q_3k = q_3
+        # else:
+        #     q_3 = abs(math.sqrt(-(1-r11-r22+r33)/4))
+        #     q_3k = -(math.sqrt(-(1-r11-r22+r33)/4))
             
-        q_lst = [q_0, q_1, q_2, q_3]
-        max_key, max_value = self.max(q_list=q_lst)
+        # q_lst = [q_0, q_1, q_2, q_3]
+        # q_lst1 = [q_0k, q_1k, q_2k, q_3k]
+        # max_key, max_value = self.max(q_list=q_lst)
 
-        if max_value == q_lst[0]:
-            q0 = q_lst[0]
-            q1 = ((r32-r23)/(4*q_lst[0]))
-            q2 = ((r13-r31)/(4*q_lst[0]))
-            q3 = ((r21-r12)/(4*q_lst[0]))
-        elif max_value == q_lst[1]:
-            q0 = ((r32-r23)/(4*q_lst[1]))
-            q1 = q_lst[1]
-            q2 = ((r12+r21)/(4*q_lst[1]))
-            q3 = ((r13+r31)/(4*q_lst[1]))
-        elif max_value == q_lst[2]:
-            q0 = ((r13-r31)/(4*q_lst[2]))
-            q1 = ((r12+r21)/(4*q_lst[2]))
-            q2 = q_lst[1]
-            q3 = ((r23+r32)/(4*q_lst[2]))
-        elif max_value == q_lst[3]:
-            q0 = ((r21-r12)/(4*q_lst[3]))
-            q1 = ((r13+r31)/(4*q_lst[3]))
-            q2 = ((r23+r32)/(4*q_lst[3]))
-            q3 = q_lst[3]
+        # if max_value == q_lst[0]:
+        #     q0 = q_lst[0]
+        #     q1 = ((r32-r23)/(4*q_lst[0]))
+        #     q2 = ((r13-r31)/(4*q_lst[0]))
+        #     q3 = ((r21-r12)/(4*q_lst[0]))
+        # elif max_value == q_lst[1]:
+        #     q0 = ((r32-r23)/(4*q_lst[1]))
+        #     q1 = q_lst[1]
+        #     q2 = ((r12+r21)/(4*q_lst[1]))
+        #     q3 = ((r13+r31)/(4*q_lst[1]))
+        # elif max_value == q_lst[2]:
+        #     q0 = ((r13-r31)/(4*q_lst[2]))
+        #     q1 = ((r12+r21)/(4*q_lst[2]))
+        #     q2 = q_lst[1]
+        #     q3 = ((r23+r32)/(4*q_lst[2]))
+        # elif max_value == q_lst[3]:
+        #     q0 = ((r21-r12)/(4*q_lst[3]))
+        #     q1 = ((r13+r31)/(4*q_lst[3]))
+        #     q2 = ((r23+r32)/(4*q_lst[3]))
+        #     q3 = q_lst[3]
+        # return q0, q1, q2, q3
+    
+        r11, r12, r13 = self.rotation_matrix[0, 0], self.rotation_matrix[0, 1], self.rotation_matrix[0, 2]
+        r21, r22, r23 = self.rotation_matrix[1, 0], self.rotation_matrix[1, 1], self.rotation_matrix[1, 2]
+        r31, r32, r33 = self.rotation_matrix[2, 0], self.rotation_matrix[2, 1], self.rotation_matrix[2, 2]
+
+        q_0n = ((1 + r11 + r22 + r33) / 4)
+        q_1n = ((1 - r11 + r22 - r33) / 4)
+        q_2n = ((1 - r11 - r22 + r33) / 4)
+        q_3n = ((1 + r11 - r22 - r33) / 4)
+
+        q_0 = math.sqrt(abs(q_0n)) * (1 if q_0n > 0 else -1)
+        q_1 = math.sqrt(abs(q_1n)) * (1 if q_1n > 0 else -1)
+        q_2 = math.sqrt(abs(q_2n)) * (1 if q_2n > 0 else -1)
+        q_3 = math.sqrt(abs(q_3n)) * (1 if q_3n > 0 else -1)
+
+        max_value = max(q_0, q_1, q_2, q_3)
+
+        if max_value == q_0:
+            q0 = max_value
+            q1 = (r32 - r23) / (4 * max_value)
+            q2 = (r13 - r31) / (4 * max_value)
+            q3 = (r21 - r12) / (4 * max_value)
+        elif max_value == q_1:
+            q0 = (r32 - r23) / (4 * max_value)
+            q1 = max_value
+            q2 = (r12 + r21) / (4 * max_value)
+            q3 = (r13 + r31) / (4 * max_value)
+        elif max_value == q_2:
+            q0 = (r13 - r31) / (4 * max_value)
+            q1 = (r12 + r21) / (4 * max_value)
+            q2 = max_value
+            q3 = (r23 + r32) / (4 * max_value)
+        elif max_value == q_3:
+            q0 = (r21 - r12) / (4 * max_value)
+            q1 = (r13 + r31) / (4 * max_value)
+            q2 = (r23 + r32) / (4 * max_value)
+            q3 = max_value
+
         return q0, q1, q2, q3
     
     def max(self, q_list:list):
