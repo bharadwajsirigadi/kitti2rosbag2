ef3d878b0892a40a7f185a08ff654b0a552a6910
bharadwajsirigadi <bharadwajsirigadi@gmail.com>
2023-11-26 21:36:23 -0500

    launch update in setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
 
 package_name = 'kitti_odometry_bag_generator'
 
@@ -10,6 +12,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name, ['launch/kitti2rosbag2.py'])
     ],
     install_requires=['setuptools'],
     zip_safe=True,
